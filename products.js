bf0e5be249a30591a82def23536719c6029d4638
Salihu Aminu Yakudima <yakudimajnr@gmail.com>
2020-11-19 10:30:46 +0100

    add lesson details

---
+++ products.js
@@ -0,0 +1,98 @@
+let products = [
+    {
+        "id": 1001,
+        "subject": 'Business',
+        "location": "Dubai",
+        "price": 30,
+        "image": "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Falejandrocremades%2Ffiles%2F2018%2F12%2Fbusiness-3605367_1920-1200x741.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1002,
+        "subject": 'Medicine',
+        "location": "Abu Dhabi",
+        "price": 50,
+        "image": "https://www.who.int/medicines/news/2019/aware_image680px.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1003,
+        "subject": 'Law',
+        "location": "Deira",
+        "price": 25,
+        "image": "https://www.thestatesman.com/wp-content/uploads/2020/08/LAW.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1004,
+        "subject": 'Architecture',
+        "location": "Sharjah",
+        "price": 15,
+        "image": "https://www.timeshighereducation.com/sites/default/files/styles/the_breaking_news_image_style/public/why-study-architecture_0.jpg?itok=yr8Uelyq",
+        "spaces": 5
+    },
+    {
+        "id": 1005,
+        "subject": 'Accounting',
+        "location": "Ajman",
+        "price": 12,
+        "image": "https://www.principlesofaccounting.com/wp-content/gallery/chapter-1/accounting.png",
+        "spaces":5
+    },
+    {
+        "id": 1006,
+        "subject": 'Finance',
+        "location": "Fujaira",
+        "price": 35,
+        "image": "https://miro.medium.com/max/1400/1*6wN_HVozHA06zj_G1bHqCA.jpeg",
+        "spaces":5
+    },
+    {
+        "id": 1007,
+        "subject": 'Arts',
+        "location": "Al Ain",
+        "price": 19,
+        "image": "https://ensia.com/wp-content/uploads/2018/01/Voices_Arts_main-760x378.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1008,
+        "subject": 'Humanities',
+        "location": "Ras Al Khaima",
+        "price": 20,
+        "image": "https://www.insidehighered.com/sites/default/server_files/styles/medium/public/media/humanities%20centers_0.jpg?itok=NbTRZ9ah",
+        "spaces":5
+    },
+    {
+        "id": 1009,
+        "subject": 'Economics',
+        "location": "Jebel Ali",
+        "price": 19,
+        "image": "https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_272/understanding_economics_1200x627.jpg?quality=89&w=1200",
+        "spaces":5
+    },
+    {
+        "id": 1010,
+        "subject": 'IT',
+        "location": "Kalba",
+        "price": 20,
+        "image": "https://i0.wp.com/marketbusinessnews.com/wp-content/uploads/2018/11/Information-Technology.jpg?w=1044&ssl=1",
+        "spaces": 5
+    },
+    {
+        "id": 1011,
+        "subject": 'Engineering',
+        "location": "Hatta",
+        "price": 13,
+        "image": "https://www.topuniversities.com/sites/default/files/styles/lead_article_image/public/blogs/lead-images/engineering_imagery.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1012,
+        "subject": 'Business Technology',
+        "location": "Al Madam",
+        "price": 25,
+        "image": "http://www.techconnections.org/wp-content/uploads/2018/10/tech-for-biz.jpg",
+        "spaces":5
+    }
+]
